258c952b78cd3af33a7f7bfe03fe3cc3b418c6de
Domenico Sibilio <domenico@fedora.homenet.telecomitalia.it>
2019-06-19 20:18:25 +0200

    Initial commit

---
+++ src/main/java/com/mizar/wannabegoogle/controllers/WannabeGoogleController.java
@@ -0,0 +1,22 @@
+package com.mizar.wannabegoogle.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WannabeGoogleController {
+
+	@GetMapping(value = "/search", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String search(@RequestParam String q) {
+		switch (q.toLowerCase()) {
+		case "gherkin":
+			return "cucumber";
+		case "george martin":
+			return "game of thrones";
+		default:
+			return "";
+		}
+	}
+}
